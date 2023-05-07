98a38dc4814d23bc7a3b7f3f9265fe99b40fc992
RohendoJ <rohendoj3134.github@gmail.com>
2023-05-07 09:48:47 +0700

    done tugas 1

---
+++ pertemuan-1/tugas_1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rohendo Junaedin")
+	fmt.Println("Jl.Sunda")
+	fmt.Println("Universitas Islam Nusantara")
+	fmt.Println("Ngoding")
+	fmt.Println("Motivasi belajar ngoding karena tertarik di bidang Software development")
+}
